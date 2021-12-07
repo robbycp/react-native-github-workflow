9ced02c559da62363494f44ff2090effd6a4831a
robby <dev.robbycaesar@gmail.com>
2021-12-07 22:08:32 +0700

    [modify] package.json version

--- scriptGithub/createPullRequest.js
+++ scriptGithub/createPullRequest.js
@@ -12,7 +12,7 @@ module.exports = async ({context, exec, github}) => {
     const jsonString = fs.readFileSync('./package.json');
     const packageJson = JSON.parse(jsonString);
     console.log('first version', packageJson);
-    packageJson.version = semver.inc(packageJson.version, 'patch');
+    packageJson.version = finalVersion;
     const stringifyOutput = JSON.stringify(packageJson, null, 2);
     console.log('second version', stringifyOutput);
     fs.writeFileSync('./package.json', stringifyOutput);
@@ -22,7 +22,7 @@ module.exports = async ({context, exec, github}) => {
   await exec.exec('git add .');
   await exec.exec('git', [
     'commit',
-    '[MODIFY] patch version package.json to start new pull request release',
+    "'[MODIFY] patch version package.json to start new pull request release'",
   ]);
   await exec.exec('git push origin staging');
 
